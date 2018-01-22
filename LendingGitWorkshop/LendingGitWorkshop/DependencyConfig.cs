2cb4987c472ea27539668eb521b6d1ecd9cb4857
Maddox, Patrick <patrick.maddox@centare.com>
2018-01-22 15:25:05 -0600

    initial setup. Added DI. Working on setting up concept and flow.

---
+++ LendingGitWorkshop/LendingGitWorkshop/DependencyConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace LendingGitWorkshop
+{
+    public static class DependencyConfig
+    {
+        public static IContainer RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+
+            //Template for registering components with AutoFac
+            //builder.RegisterType<PutAClassNameHere>().As<PutTheClass'sInterfaceHere>();
+
+            builder.RegisterType<Application>().As<IApplication>();
+
+            //You can put more 
+
+            return builder.Build();
+        }
+    }
+}
--- LendingGitWorkshop/LendingGitWorkshop/Program.cs
+++ LendingGitWorkshop/LendingGitWorkshop/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,20 @@ namespace LendingGitWorkshop
 {
     class Program
     {
+        //This happens before Main is called!
+        private static IContainer Container { get; } = DependencyConfig.RegisterDependencies();
+
         static void Main(string[] args)
         {
+            //Establish the scope for the dependency tree
+            using(var scope = Container.BeginLifetimeScope())
+            {
+                //Will into existence the magnificent dependency tree exist
+                var application = scope.Resolve<IApplication>();
 
+                //Actually run the program
+                application.Run();
+            }
         }
     }
 }
